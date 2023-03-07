c19f215f790a927664a4026491851b29279222ec
DionisSerbin <reaganusm@gmail.com>
2023-03-07 18:10:45 +0300

    сверстал экран регистрации

---
+++ app/src/main/java/com/example/kotkin_team/authentication/presentation/view/SignInFragment.kt
@@ -0,0 +1,20 @@
+package com.example.kotkin_team.authentication.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kotkin_team.R
+
+
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+}
