448aa65c404bed3bb9286590aa73fd8e9f20f7c1
DionisSerbin <reaganusm@gmail.com>
2023-03-07 18:35:43 +0300

    добавил логику регистрации

--- app/src/main/java/com/example/kotkin_team/MainActivity.kt
+++ app/src/main/java/com/example/kotkin_team/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotkin_team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getFirebaseAuthInstance(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
     }
 }
--- app/src/main/java/com/example/kotkin_team/authentication/presentation/view/SignUpFragment.kt
+++ app/src/main/java/com/example/kotkin_team/authentication/presentation/view/SignUpFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kotkin_team.MainActivity
 import com.example.kotkin_team.R
+import com.google.android.material.textfield.TextInputEditText
 
 
 class SignUpFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +23,54 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val alreadyReg = view.findViewById<TextView>(R.id.already_reg)
+        alreadyReg.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        val signUpButton = view.findViewById<Button>(R.id.sign_up_button)
+
+        signUpButton.setOnClickListener {
+            val email = view.findViewById<TextInputEditText>(R.id.email_text).text.toString()
+            val pass = view.findViewById<TextInputEditText>(R.id.password_text).text.toString()
+            val confirmPass =
+                view.findViewById<TextInputEditText>(R.id.confirm_password_text).text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+                    (activity as MainActivity).getFirebaseAuthInstance()
+                        .createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Toast.makeText(
+                                    signUpButton.context,
+                                    "${email} успешно добавлен в аккаунт",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+                            } else {
+                                Toast.makeText(
+                                    signUpButton.context,
+                                    it.exception.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(signUpButton.context, WRONG_PASSWORD_REPEAT, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(signUpButton.context, WRONG_DATA, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    companion object {
+        private const val WRONG_DATA = "Вы не ввели все данные"
+        private const val WRONG_PASSWORD_REPEAT = "Пароли не совпадают"
+    }
 }
